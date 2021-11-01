d4b49711a16aa615ba3f25d05b071d3702f6be77
Maria Gornashevich <m.harnashevich@tapston.com>
2021-11-02 00:07:39 +0300

    Created hbs

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+
 const app = express();
+
+//setting handlebars
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine); // register engine
+app.set("view engine", "hbs"); // use engine
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 const PORT = process.env.PORT || 3000;
 
